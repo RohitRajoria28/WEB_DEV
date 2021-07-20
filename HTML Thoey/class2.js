6cdb9b7501eb73cf8bff6322f75e89bdd6d28eb8
RohitRajoria28 <55023373+RohitRajoria28@users.noreply.github.com>
2021-07-20 20:40:42 +0530

    update

---
+++ HTML Thoey/class2.js
@@ -0,0 +1,16 @@
+// for (let i = 0; i < 5; i++) {
+
+//     console.log(i);
+
+
+
+// }
+let a={
+    "rohit":"Simran",
+    "simran":"i love you",
+    1:40
+
+}
+console.log(a["rohit"]);
+console.log(a["simran"]);
+console.log(a[1]);
